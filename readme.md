37524dd1ef9908c38d35dd860b3b34ed991b58ff
Daniel Ale <denielale9291@gmail.com>
2019-04-24 13:00:20 +0100

    Complete webpack setup

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 # My version of create react app
+
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style/main.less';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style/main.less'
+import App from './App'
 
-class Welcome extends React.Component {
-    render () {
-        return <h1>McDan React boilerplate</h1>;
-    }
-}
-ReactDOM.render(<Welcome />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
